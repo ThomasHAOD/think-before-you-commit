c4857ed543eddb27b90645db1e59759137710c69
Tam O'Donnell <thomashodonnell@gmail.com>
2021-01-31 10:50:34 +0000

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const cloudServiceKey = 'd9834jhd98n39dns9dnxx0356nm';
 
+const cloudServiceDatabaseID = '3856732272937haksjkdj';
+
 const exampleAdminUser = {
   name: 'Jane "The CEO" Jenkins',
   username: 'imceo',
@@ -9,6 +11,6 @@ const exampleAdminUser = {
   password: 'qwerty1234',
 };
 
-const server = http.createServer();
+const server = http.connectToDatabase(cloudServiceDatabaseID, cloudServiceKey);
 
-server.saveToDataBase(exampleAdminUser, cloudServiceKey);
+server.saveToDataBase(exampleAdminUser);
